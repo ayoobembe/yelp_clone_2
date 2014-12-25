3cf40fa9039d7c9e4cafba5f2860ef1e7dae983b
danielobembe <ayo.obembe@gmail.com>
2014-12-25 07:17:44 +0000

    Added restaurant model, modified controller and views to respond to database

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
 	def index
 		# why does this go to restaurants/index.html.erb without being told
+		@restaurants = Restaurant.all
 	end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,9 +5,21 @@ feature 'Restaurants:' do
 	context 'When no restaurants have been added' do 
 		scenario 'it should display a prompt to add a restaurant' do 
 			visit '/restaurants'
-			expect(page).to have_content 'No restaurants'
+			expect(page).to have_content 'No restaurants yet'
 			expect(page).to have_link 'Add a restaurant'
 		end
 	end
+
+	context 'When restaurants have been added' do 
+		before do 
+			Restaurant.create(name: 'KFC')
+		end
+		scenario 'it should display the restaurants' do 
+			visit '/restaurants'
+			expect(page).to have_content('KFC')
+			expect(page).not_to have_content('No restaurants yet')
+		end
+	end
+
 
 end
